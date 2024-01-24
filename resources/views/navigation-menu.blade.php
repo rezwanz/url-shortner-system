3ba5a18952c206ea8b850e84618fa744a86978c7
Md. Rejwan Mahmud <mrm.shakil@yahoo.com>
2024-01-24 23:26:26 +0600

    Nav Updated

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('generate-shorten-link') }}" :active="request()->routeIs('generate-shorten-link')">
+                        {{ __('URL Shortner') }}
+                    </x-nav-link>
                 </div>
             </div>
 
